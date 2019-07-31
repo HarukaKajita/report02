075b764471418ffd63c90ce976736a1e995cbfde
HarukaKajita <kajitaj63b3@gmail.com>
2019-07-31 21:30:44 +0900

    コードfix

--- report02/report02.cpp
+++ report02/report02.cpp
@@ -5,50 +5,6 @@
 
 using namespace std;
 
-// セルを表わす構造体の定義 
-struct cell
-{
-	string name;   // 名前
-	string tel;    // 電話番号
-
-	struct cell* next;	// 次のデータへのポインタ
-};
-
-struct cell* make_cell(string name, string tel)
-{
-	struct cell* mk_cell;
-
-	// cellの領域を確保する 
-	mk_cell = new struct cell;
-
-	mk_cell->name = name;
-	mk_cell->tel = tel;
-
-	// nextを初期化
-	mk_cell->next = NULL;
-
-	return mk_cell;
-}
-
-
-string search_tel(struct cell* head, string name)
-{
-	struct cell* buf;
-	buf = head;
-
-	while (buf != NULL)
-	{
-		if (buf->name == name)
-		{
-			return buf->tel;
-		}
-
-		buf = buf->next;
-	}
-
-	return "none";
-}
-
 // ファイルの要素数を取得
 int get_nx(char* f_name)
 {
@@ -74,89 +30,52 @@ int get_nx(char* f_name)
 	return nx;
 }
 
-template<class T>
-void printArray(T array[], int n) {
-	cout << "==============================" << endl;
-	for (int i = 0; i < n; i ++) {
-		cout << "[" << i << "] : " << array[i] << endl;
-	}
-	cout << "==============================" << endl;
+void exchange_pVal(string* a, string* b)
+{
+	string tmp;
 
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
-// アルファベット順にソート
-void name_sort(string name_data[], int n)
+void quick_sort(string a[], int left, int right)
 {
-	//クイックソート
-#ifdef _DEBUG
-	cout << "ソート開始" << endl;
-	printArray(name_data, n);
-#endif // _DEBUG
+	int index1 = left ;
+	int index2 = right;
+	string x = a[left];
 
-	if (n == 2) {
-		if(name_data[0] > name_data[1])swap(name_data[0], name_data[1]);
-#ifdef _DEBUG
-		cout << "ソート終了" << endl;
-		printArray(name_data, n);
-#endif // _DEBUG
-		return;
+	do
+	{
+		while (a[index1] < x)
+		{
+			index1++;
+		}
+
+		while (a[index2] > x)
+		{
+			index2--;
+		}
+
+		if (index1 <= index2)
+		{
+			exchange_pVal(&a[index1], &a[index2]);
+			index1++;
+			index2--;
+		}
+	} while (index1 < index2);
+
+	if (left < index2) {
+		exchange_pVal(&a[left], &a[index2]);
 	}
-	else if (n <= 1) {
-#ifdef _DEBUG
-		cout << "ソート終了" << endl;
-		printArray(name_data, n);
-#endif // _DEBUG
-		return;
+
+	if (left < index2) {
+		quick_sort(a, left, index2);
 	}
 
-	string baseString = name_data[0];
-#ifdef _DEBUG
-	cout << "base : " << baseString << endl;
-
-#endif
-	int largeIndex;
-	int smallIndex;
-	while (true) {
-		for (largeIndex=1; largeIndex < n; largeIndex++) {
-			string large = name_data[largeIndex];
-			if (large > baseString) {
-#ifdef _DEBUG
-				cout << "large : " << large << endl;
-#endif
-				break;
-			}
-		}
-		for (smallIndex=n-1; smallIndex > 0; smallIndex--) {
-			string small = name_data[smallIndex];
-			if (small < baseString) {
-#ifdef _DEBUG 
-				cout << "small : " << small << endl;
-#endif
-				break;
-			}
-		}
-
-		if (smallIndex < largeIndex) {
-			swap(name_data[0], name_data[smallIndex]);
-			break;
-		}
-		else {
-			if (smallIndex > 0) {
-				swap(name_data[smallIndex], name_data[largeIndex]);
-			}
-		}
+	if (index1 < right) {
+		quick_sort(a, index1, right);
 	}
-#ifdef _DEBUG
-	cout << "ソート終了" << endl;
-	printArray(name_data, n);
-#endif // _DEBUG
-	//0~smallIndex
-	cout << "sort left" << endl;
-	if (smallIndex > 0)name_sort(name_data, smallIndex + 1);
-	//largeIndex~n-1
-	cout << "sort right" << endl;
-	if (largeIndex < n)name_sort(name_data + largeIndex, n - largeIndex);
-	
 }
 
 int hashfunction(string key, int size) {
@@ -198,92 +117,18 @@ struct customHash
 		h->table[hash_value] = temp;
 	}
 
-	node* search(customHash* h, string key) {
+	string search(customHash* h, string key) {
 		int hash_value = hashfunction(key, h->size);
 		node* p = h->table[hash_value];
 		while (p != NULL) {
 			if (p->key == key) {
-				return p;
+				return p->value;
 			}
 			p = p->next;
 		}
-		return NULL;
+		return "none";
 	}
 };
-
-//int main(int argc, char* argv[])
-//{
-//	if (argc == 1)  // 引数がない場合
-//	{
-//		cout << "input file name\n";
-//		return -1;
-//	}
-//
-//	ifstream fp(argv[1]);   // ファイルオープン
-//	//ファイルオープン失敗でエラー出力し異常終了
-//	if (!fp) {
-//		cerr << "failed to open file" << endl;
-//		return -1;
-//	}
-//	// リストのヘッドを定義し初期化
-//	struct cell* head;
-//	head = NULL;
-//
-//	int nx = get_nx(argv[1]);   // ファイルの要素数を取得
-//	
-//	//ハッシュの初期化
-//	customHash* hash_data = new customHash;
-//	hash_data->initialize(hash_data, nx);
-//
-//#ifdef _DEBUG
-//	cout << "データ要素数：" << nx << endl;
-//#endif // _DEBUG
-//	string* name_data;
-//	name_data = new string[nx];  // 名前を格納
-//
-//	//#ifdef _DEBUG
-//	cout << "データ構造構築開始" << endl;
-//	//#endif // _DEBUG
-//
-//	while (fp >> line)
-//	{
-//		// 名前と電話番号に分割
-//		int index = line.find(",");
-//		string name = line.substr(0, index);
-//		string tel = line.substr(index + 1);
-//
-//		hash_data->add(hash_data, name, tel);
-//	}
-//	fp.close();
-//
-//	//#ifdef _DEBUG
-//	cout << "データ構造構築開始" << endl;
-//	cout << "ソート開始" << endl;
-//	//#endif // _DEBUG
-//
-//		// 名前をアルファベット順にソート
-//	name_sort(name_data, nx);
-//
-//	//#ifdef _DEBUG
-//	cout << "ソート終了" << endl;
-//	cout << "全表示開始" << endl;
-//	//#endif // _DEBUG
-//
-//		// 全表示
-//	for (int ky = 0; ky < nx; ky++)
-//	{
-//		string tel = hash_data->search(hash_data, )
-//		string tel = search_tel(head, name_data[ky]);
-//		cout << name_data[ky] << " -> " << tel << "\n";
-//	}
-//
-//	//#ifdef _DEBUG
-//	cout << "全表示終了" << endl;
-//	//#endif // _DEBUG
-//
-//	return 0;
-//}
-
 
 int main(int argc, char* argv[])
 {
@@ -304,19 +149,22 @@ int main(int argc, char* argv[])
 	head = NULL;
 
 	int nx = get_nx(argv[1]);   // ファイルの要素数を取得
-#ifdef _DEBUG
+	
+	//ハッシュの初期化
+	customHash* hash_data = new customHash;
+	hash_data->initialize(hash_data, nx);
+
+	#ifdef _DEBUG
 	cout << "データ要素数：" << nx << endl;
-#endif // _DEBUG
+	#endif // _DEBUG
 	string* name_data;
 	name_data = new string[nx];  // 名前を格納
 
-	string line;
-	int i = 0;
-
-//#ifdef _DEBUG
+	#ifdef _DEBUG
 	cout << "データ構造構築開始" << endl;
-//#endif // _DEBUG
-
+	#endif // _DEBUG
+	string line;
+	int count = 0;
 	while (fp >> line)
 	{
 		// 名前と電話番号に分割
@@ -324,42 +172,47 @@ int main(int argc, char* argv[])
 		string name = line.substr(0, index);
 		string tel = line.substr(index + 1);
 
-		// リストに名前と電話番号を追加
-		struct cell* e_cell;
-		e_cell = make_cell(name, tel);
-
-		e_cell->next = head;
-		head = e_cell;
-
-		// 名前を配列に格納
-		name_data[i] = name;
-		i++;
+		hash_data->add(hash_data, name, tel);
+		name_data[count++] = name;
 	}
 	fp.close();
 
-//#ifdef _DEBUG
+	#ifdef _DEBUG
 	cout << "データ構造構築開始" << endl;
 	cout << "ソート開始" << endl;
-//#endif // _DEBUG
+	#endif // _DEBUG
 
-	// 名前をアルファベット順にソート
-name_sort(name_data, nx);
+		// 名前をアルファベット順にソート
+	quick_sort(name_data, 0, nx-1);
 
-//#ifdef _DEBUG
+	#ifdef _DEBUG
 	cout << "ソート終了" << endl;
 	cout << "全表示開始" << endl;
-//#endif // _DEBUG
+	#endif // _DEBUG
 
 	// 全表示
-	for (int ky = 0; ky < nx; ky++)
+	/*for (int ky = 0; ky < nx; ky++)
 	{
-		string tel = search_tel(head, name_data[ky]);
+		string tel = hash_data->search(hash_data, name_data[ky]);
+		cout << name_data[ky] << " -> " << tel << "\n";
+	}*/
+
+	for (int ky = 0; ky < 5; ky++)
+	{
+		string tel = hash_data->search(hash_data, name_data[ky]);
 		cout << name_data[ky] << " -> " << tel << "\n";
 	}
 
-//#ifdef _DEBUG
+	for (int ky = 0; ky < 5; ky++)
+	{
+		int index = nx - 1 - ky;
+		string tel = hash_data->search(hash_data, name_data[index]);
+		cout << name_data[index] << " -> " << tel << "\n";
+	}
+
+	#ifdef _DEBUG
 	cout << "全表示終了" << endl;
-//#endif // _DEBUG
+	#endif // _DEBUG
 
 	return 0;
 }
